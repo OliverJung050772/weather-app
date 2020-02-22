30962f877bab0ea513748eaee65d23ddbcf5bd57
OliverJung050772 <oliver.jung@gmx.tm>
2020-02-22 01:46:05 +0100

    view and functionality

--- weather-app/src/app/temperature/temperature.component.ts
+++ weather-app/src/app/temperature/temperature.component.ts
@@ -5,18 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './temperature.component.html',
   styleUrls: ['./temperature.component.css']
 })
+
 export class TemperatureComponent implements OnInit {
 
   temperature: number;
+  pressure: number;
 
   constructor() { }
 
-  onMeasureClick(): void {
+  onMeasureTemperatureClick(): void {
     this.temperature = Math.round(60 * Math.random() - 20);
+  }
+
+  onMeasurePressureClick(): void {
+    this.pressure = Math.round(70 * Math.random() + 1080);
   }
 
   ngOnInit(): void {
     this.temperature = 0;
+    this.pressure = 0;
   }
 
 }
