6b1b6bf87222d572106236fdbb9f5868abd245b8
Oliver Jung <oliver.jung@xitaso.com>
2020-02-25 16:17:28 +0100

    hourly measurement of pressure

--- weather-app/src/app/components/pressure/pressure.component.ts
+++ weather-app/src/app/components/pressure/pressure.component.ts
@@ -9,17 +9,27 @@ import { WeatherService } from '../../services/weather.service';
 export class PressureComponent implements OnInit {
 
   currentPressure: number;
+  measuredPressures: number[] = [];
+  measuredPressuresPerHour: number[] = [];
+  trendPressure: number;
 
   constructor(private weatherService: WeatherService) { }
 
   measurePressure(): void {
     this.currentPressure = this.weatherService.getPressure();
+    this.measuredPressures.push(this.currentPressure);
+  }
+
+  measurePressureOfHours(): void {
+    this.trendPressure = this.weatherService.getPressure();
+    this.measuredPressuresPerHour.push(this.trendPressure);
   }
 
   ngOnInit(): void {
     setInterval(() => this.measurePressure(), 5000);
     // set initial pressure-value
     this.measurePressure();
+    setInterval(() => this.measurePressureOfHours(), 60000);
   }
 
 }
