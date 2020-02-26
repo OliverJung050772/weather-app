a8f949972cb8ecab4d8abba2cafbf7324ab1b3ff
Oliver Jung <oliver.jung@xitaso.com>
2020-02-26 11:44:54 +0100

    implemented pressure-trend

--- weather-app/src/app/components/pressure/pressure.component.ts
+++ weather-app/src/app/components/pressure/pressure.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService } from '../../services/weather.service';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from '../../services/weather.service';
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-pressure',
@@ -9,27 +10,50 @@ import { WeatherService } from '../../services/weather.service';
 export class PressureComponent implements OnInit {
 
   currentPressure: number;
-  measuredPressures: number[] = [];
-  measuredPressuresPerHour: number[] = [];
-  trendPressure: number;
+  currentHourlyPressure: number;
+  pressureTrendSymbol: string;
+  pressureTrendText: string;
+  private readonly intervalShort: number = 5000;
+  private readonly intervalLong: number = 15000;
+  private readonly pressureChangeSubject = new Subject<number>();
+  private readonly pressureTrendChangeSubject = new Subject<number>();
 
-  constructor(private weatherService: WeatherService) { }
-
-  measurePressure(): void {
-    this.currentPressure = this.weatherService.getPressure();
-    this.measuredPressures.push(this.currentPressure);
+  constructor(private weatherService: WeatherService) {
   }
 
-  measurePressureOfHours(): void {
-    this.trendPressure = this.weatherService.getPressure();
-    this.measuredPressuresPerHour.push(this.trendPressure);
+  public measurePressure(): void {
+    this.pressureChangeSubject.next(this.weatherService.getPressure());
+  }
+
+  private measurePressureOfHours(): void {
+    this.pressureTrendChangeSubject.next(this.weatherService.getPressure());
+  }
+
+  private showPressureTrend(newPressure: number): void {
+    const deltaPressure = newPressure - this.currentHourlyPressure;
+    const timeSpan = this.intervalLong / (1000 * 6);
+    if (Math.abs(deltaPressure) >= 4 && Math.abs(deltaPressure) / timeSpan >= 10) {
+      if (deltaPressure > 0) {
+        this.pressureTrendText = 'rising';
+        this.pressureTrendSymbol = '↑';
+      } else {
+        this.pressureTrendText = 'falling';
+        this.pressureTrendSymbol = '↓';
+      }
+    } else {
+      this.pressureTrendText = 'stable';
+      this.pressureTrendSymbol = '→';
+    }
+    this.currentHourlyPressure = newPressure;
   }
 
   ngOnInit(): void {
-    setInterval(() => this.measurePressure(), 5000);
     // set initial pressure-value
-    this.measurePressure();
-    setInterval(() => this.measurePressureOfHours(), 60000);
+    this.currentPressure = this.weatherService.getPressure();
+    setInterval(() => this.measurePressure(), this.intervalShort);
+    setInterval(() => this.measurePressureOfHours(), this.intervalLong);
+    this.pressureChangeSubject.asObservable().subscribe( value => this.currentPressure = value);
+    this.pressureTrendChangeSubject.asObservable().subscribe(value => this.showPressureTrend(value));
   }
 
 }
--- weather-app/src/app/components/temperature/temperature.component.ts
+++ weather-app/src/app/components/temperature/temperature.component.ts
@@ -7,34 +7,38 @@ import {Subject} from 'rxjs';
   templateUrl: './temperature.component.html',
   styleUrls: ['./temperature.component.css']
 })
-
 export class TemperatureComponent implements OnInit {
 
   currentTemperature: number;
   averageTemperature: number;
-
   measuredTemperatures: number[] = [];
-  private readonly subject = new Subject<number[]>();
+  private readonly  measureInterval = 7000;
+  private readonly averageTemperatureSubject = new Subject<number[]>();
+  private readonly actualTemperatureSubject = new Subject<number>();
 
   constructor(private weatherService: WeatherService) {
   }
 
-  measureTemperature(): void {
-    this.currentTemperature = this.weatherService.getTemperature();
-    this.measuredTemperatures.push(this.currentTemperature);
-    this.subject.next(this.measuredTemperatures);
+  public measureTemperature(): void {
+    const newTemperature = this.weatherService.getTemperature();
+    this.measuredTemperatures.push(newTemperature);
+    this.averageTemperatureSubject.next(this.measuredTemperatures);
+    this.actualTemperatureSubject.next(newTemperature);
+  }
+
+  private updateAverageTemperature(temperatureArray: number[]): void {
+    const arraySum = temperatureArray.reduce((a, b) => a + b, 0);
+    const arrayLength = temperatureArray.length;
+    this.averageTemperature = Math.round(arraySum / arrayLength);
   }
 
   ngOnInit(): void {
-    setInterval(() => this.measureTemperature(), 10000);
     // Set initial Temperature
     this.averageTemperature = 0;
-    this.measureTemperature();
-    this.subject.asObservable().subscribe((array) => {
-        const arraySum = array.reduce((a, b) => a + b, 0);
-        const arrayLength = array.length;
-        this.averageTemperature = Math.round(arraySum / arrayLength);
-    });
+    this.currentTemperature = this.weatherService.getTemperature();
+    setInterval(() => this.measureTemperature(), this.measureInterval);
+    this.averageTemperatureSubject.asObservable().subscribe((array) => this.updateAverageTemperature(array));
+    this.actualTemperatureSubject.asObservable().subscribe(value => this.currentTemperature = value);
   }
 
 }
